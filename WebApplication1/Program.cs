9ddbccd9f181d0ddf79fc4e0e5c686fd2d1ac993
JaimeM <jaime.mendez@simpat.tech>
2025-06-24 17:03:19 -0600

    Fixing swagger

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -38,11 +38,8 @@ builder.Services.AddAutoMapper(typeof(MappingProfile));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
